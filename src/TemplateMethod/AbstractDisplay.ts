9cd449f4e95864e188de83a515881c7bb0c5f037
motohiro ishii <m.ece0113@gmail.com>
2019-04-03 23:48:21 +0900

    add AbstractDisplay

---
+++ src/TemplateMethod/AbstractDisplay.ts
@@ -0,0 +1,21 @@
+export abstract class AbstractDisplay {
+
+    private loopCount: number = 5;
+
+    constructor() {
+    }
+
+    public abstract open(): void;
+    public abstract print(): void;
+    public abstract close(): void;
+
+    public display(): void {
+        this.open();
+
+        for (let index = 0; index < this.loopCount; index++) {
+            print();
+        }
+        close();
+    }
+
+}
