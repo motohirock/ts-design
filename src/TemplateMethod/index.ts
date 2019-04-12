0689ffad0c7835b31bc9c70836713dd13dcf1672
motohiro ishii <m.ece0113@gmail.com>
2019-04-12 23:58:10 +0900

    update TemplateMethod:index

--- src/TemplateMethod/index.ts
+++ src/TemplateMethod/index.ts
@@ -2,11 +2,12 @@ import { AbstractDisplay } from "./AbstractDisplay";
 import { CharDisplay } from "./CharDisplay";
 import { StringDisplay } from "./StringDisplay";
 
-var d1: AbstractDisplay = new CharDisplay("H");
+var d1: AbstractDisplay = new CharDisplay("ttttttttt");
 
-var d2: AbstractDisplay = new StringDisplay("Hello");
+var d2: AbstractDisplay = new StringDisplay("tttttttttttttttt");
 
-var d3: AbstractDisplay = new StringDisplay("beroberobero")
+var d3: AbstractDisplay = new StringDisplay("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
 d1.display();
 d2.display();
 d3.display();
