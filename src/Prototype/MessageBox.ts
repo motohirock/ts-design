5d0ee70a1016069257ac42539a1c0d828d5a5a40
motohiro ishii <m.ece0113@gmail.com>
2019-04-24 22:45:35 +0900

    update mb

--- src/Prototype/MessageBox.ts
+++ src/Prototype/MessageBox.ts
@@ -3,6 +3,10 @@ import clone from 'clone';
 export class MessageBox implements IProduct {
     private char: string;
 
+    constructor(char: string) {
+        this.char = char;
+    }
+
     use(s: string): void {
         console.log("\"" + s + "\"");
         console.log(Array(s.length + 3).join(this.char));
