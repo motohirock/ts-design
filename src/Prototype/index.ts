f1178dcfa55ca27c048101a2239aa4a79e6bfc23
motohiro ishii <m.ece0113@gmail.com>
2019-04-26 23:40:46 +0900

    update index.ts

--- src/Prototype/index.ts
+++ src/Prototype/index.ts
@@ -8,7 +8,13 @@ var messageBox: MessageBox = new MessageBox("*");
 var messageBox2: MessageBox = new MessageBox("/");
 
 manager.register("strong message", underlinePen);
+manager.register("asterisk box", messageBox);
+manager.register("slash box", messageBox2);
 
 var p1: IProduct = manager.create("strong message");
+var p2: IProduct = manager.create("asterisk box");
+var p3: IProduct = manager.create("slash box")
 
-p1.use("aaaaaaaaaaaaaaaaaaaaaa");
+p1.use("hello world");
+p2.use("hello world");
+p3.use("Hello World");
