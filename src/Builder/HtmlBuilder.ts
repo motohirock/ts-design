34d3e5994b390a531d26d0e40e36560945e2dc73
motohiro ishii <m.ece0113@gmail.com>
2019-04-29 23:33:01 +0900

    add HtmlBuilder

---
+++ src/Builder/HtmlBuilder.ts
@@ -0,0 +1,15 @@
+import { Builder } from "./Builder";
+
+export class HtmlBuilder extends Builder {
+    public makeTitle(title: string): void {
+        throw new Error("Method not implemented.");
+    } public makeString(str: string): void {
+        throw new Error("Method not implemented.");
+    }
+    public makeItems(items: {}): void {
+        throw new Error("Method not implemented.");
+    }
+    public close(): void {
+        throw new Error("Method not implemented.");
+    }
+}
