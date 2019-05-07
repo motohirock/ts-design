8a9752f14283000d0a4847f91ce5fe530cecec1b
motohiro ishii <m.ece0113@gmail.com>
2019-05-07 23:59:37 +0900

    update komagoma

--- src/Builder/HtmlBuilder.ts
+++ src/Builder/HtmlBuilder.ts
@@ -2,7 +2,8 @@ import { Builder } from "./Builder";
 export class HtmlBuilder extends Builder {
     public makeTitle(title: string): void {
         throw new Error("Method not implemented.");
-    } public makeString(str: string): void {
+    }
+    public makeString(str: string): void {
         throw new Error("Method not implemented.");
     }
     public makeItems(items: {}): void {
