019de50b6fb0a15fffa5464b16c6e42d26b3ccec
motohiro ishii <m.ece0113@gmail.com>
2019-05-12 23:59:25 +0900

    update

--- src/Builder/TextBuilder.ts
+++ src/Builder/TextBuilder.ts
@@ -1,8 +1,14 @@
 import { Builder } from "./Builder";
+
 export class TextBuilder extends Builder {
+
+    private buffer: string;
+
     public makeTitle(title: string): void {
-        throw new Error("Method not implemented.");
-    } public makeString(str: string): void {
+        this.buffer += "==============";
+    }
+
+    public makeString(str: string): void {
         throw new Error("Method not implemented.");
     }
     public makeItems(items: {}): void {
